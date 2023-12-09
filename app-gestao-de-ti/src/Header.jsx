ed47d6df3c8a9950bf3e45f3c8b64cf2acbd6935
Jaques <98719928+JaquesRodrigues@users.noreply.github.com>
2023-12-09 12:44:00 +0000

    🤷‍♀️🤷‍♀️🤷‍♀️🤷‍♀️

--- app-gestao-de-ti/src/Header.jsx
+++ app-gestao-de-ti/src/Header.jsx
@@ -1,15 +1,9 @@
 
 function Header() {
-    let num = 123;
-    const pessoas = {
-        nome:  "Jaques",
-        cargo: "",
-        ID: "#" + num
-    }
 
     return(
         <header>
-            <h1 className="text-center text-cyan-700 font-bold text-5xl my-8">Seja Bem-Vindo {pessoas.nome}</h1>
+            <h1 className="text-center text-cyan-700 font-bold text-5xl my-8">Bem-Vindo</h1>
         </header>
     )
 }
